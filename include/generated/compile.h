c0804e71d5f640745126dcec313224f021bd7614
LeeMenHin <1249014784@qq.com>
2019-08-22 12:08:37 +0800

    update to latest

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jul 29 23:40:42 HKT 2019"
+#define LINUX_COMPILE_BY "peter"
+#define LINUX_COMPILE_HOST "Fusion"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.9.59-FusionLeo-Kernel.V1.2+"
